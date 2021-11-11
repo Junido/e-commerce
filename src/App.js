18d9b9071b56e88e8a5741dcb464e4bbeef223ac
S844834 <julien.antonini.devoteam@msc-assurance.fr>
2021-11-11 22:54:47 +0100

    trade, price, taux

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import Navbar from './component/Navbar';
 import Char from './component/Char';
-import {Container, Box } from '@mui/material';
-import { border } from '@mui/system';
+import {Container, Box, Grid } from '@mui/material';
+
 function App() {
+
+  const style = {
+    boxShadow: 0,
+    border: '1px solid rgba(153,153,153,0.2)', 
+    borderRadius:'6px',
+    backgroundColor:'rgb(255 255 255)', 
+    padding:'15px'
+  }
+
   return (
     <div className="App">
       <Navbar />
       <Container maxWidth="lg">
-        <Box sx={{ boxShadow: 0,border: '1px solid rgba(153,153,153,0.2)', borderRadius:'6px',backgroundColor:'rgb(255 255 255)', padding:'20px' }}>
+        <Box sx={style}>
           <Char />
         </Box>
       </Container>
